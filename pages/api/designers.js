12fce67e127f8e6ad04add6e16939aa522e99b9c
milamaciel <engcamilamaciel@gmail.com>
2025-03-07 19:43:08 -0300

    Update designers.js

--- pages/api/designers.js
+++ pages/api/designers.js
@@ -23,8 +23,8 @@ export default async (req, res) => {
     // Replace the range with the tab name.
     // Issues with permissions look at this guide: https://leerob.io/snippets/google-sheets
     const response = await sheets.spreadsheets.values.get({
-      spreadsheetId: "12LLA-NoHin0zQfmpEblgMjd260bmriLMowBAH1QDOhI",
-      range: "Designers", // sheet name
+      spreadsheetId: "1nFxkc-qY7-T66yXxXfMfmnN11bHag6DrYD94mE6MKhs",
+      range: "Mulheres", // sheet name
     });
 
     //TODO: Map the collum to object name automatically.
