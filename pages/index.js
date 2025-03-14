b676e6616248430432b9c7c4f8b86fe2b445e77c
milamaciel <engcamilamaciel@gmail.com>
2025-03-14 14:02:16 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export async function getStaticProps() {
   const origin =
     process.env.NODE_ENV !== "production"
       ? "http://localhost:3000"
-      : "https://brazilianswho.design/";
+      : "https://mulheresdosetoreletrico-kee0fmvmb-camila-maciels-projects.vercel.app/";
 
   console.log(origin)
 
@@ -126,7 +126,7 @@ export default function Home({ designers, filters }) {
       }}
     >
       <Head>
-        <title>Brazilians Who Design</title>
+        <title>Mulheres no setor elétrico</title>
         <link id="favicon" rel="alternate icon" href="/favicon.ico" />
         <MetaTags />
       </Head>
@@ -183,7 +183,7 @@ function Content({ designers, handleOpenFilter, className, onClick }) {
     <div className={className} onClick={onClick}>
       <Nav />
 
-      <Title className="title m0 p0" text="Brazilians*who&nbsp;design" />
+      <Title className="title m0 p0" text="Mulheres*no&nbsp;setorelétrico" />
 
       <motion.div
         initial={{ opacity: 0 }}
